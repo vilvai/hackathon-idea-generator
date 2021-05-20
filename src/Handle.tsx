c2e973b0ee01d6c5a65757b6e39808e23e44bf71
Ville Vainio <ville.vainio@smartly.io>
2021-05-20 11:56:34 +0300

    added handle to start slot machine

---
+++ src/Handle.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 10px;
+  height: 200px;
+  border-radius: 5px;
+  background-color: black;
+  margin-left: 16px;
+  position: relative;
+`;
+
+interface HandleHeadProps {
+  position: number;
+}
+
+const HandleHead = styled.div.attrs<HandleHeadProps>(({ position }) => ({
+  style: {
+    top: position,
+  },
+}))<HandleHeadProps>`
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background-color: red;
+  position: absolute;
+`;
+
+interface Props {
+  onStart: () => void;
+  disabled: boolean;
+}
+
+const Handle = ({ onStart, disabled }: Props) => {
+  const [position, setPosition] = React.useState(0);
+
+  //const onMouseMove = (e) => {};
+
+  return (
+    <Container onClick={disabled ? undefined : onStart}>
+      <HandleHead position={position} />
+    </Container>
+  );
+};
+
+export default Handle;
--- src/SlotMachine.tsx
+++ src/SlotMachine.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import Handle from "./Handle";
 
 import Reel from "./Reel";
 
-const Container = styled.div`
+const ReelArea = styled.div`
   border: 1px solid black;
   display: flex;
 `;
 
 interface State {
-  reelData: Array<{ position: number; slots: string[] }>;
+  reelData: Array<{ position: number; slots: string[]; speed: number }>;
 }
 
 const ideas: string[][] = [
-  ["A", "B", "C"],
   ["A scalable", "A zero-down-time", "A simple"],
   ["todo", "random idea", "video", "retail"],
   ["app", "generator", "game", "machine learning model"],
@@ -22,33 +22,52 @@ const ideas: string[][] = [
 
 export default class SlotMachine extends React.Component<{}, State> {
   state = {
-    reelData: ideas.map((idea) => ({ position: 0, slots: idea })),
+    reelData: ideas.map((idea) => ({ position: 0, slots: idea, speed: 0 })),
   };
 
-  componentDidMount() {
-    this.update();
-  }
+  startSlotMachine = () => {
+    this.setState(
+      {
+        reelData: this.state.reelData.map((data, i) => ({
+          ...data,
+          speed: 25 + i * 2,
+        })),
+      },
+      this.update
+    );
+  };
 
   update() {
+    const { reelData } = this.state;
     this.setState({
-      reelData: this.state.reelData.map(({ position, ...rest }, i) => ({
-        position: position + 1 + i / 3,
-        ...rest,
+      reelData: reelData.map((data) => ({
+        ...data,
+        position: data.position + data.speed,
+        speed: Math.max(0, data.speed - 0.1),
       })),
     });
 
-    setTimeout(() => this.update(), 16);
+    if (this.isRunning) {
+      setTimeout(() => this.update(), 16);
+    }
+  }
+
+  get isRunning() {
+    return this.state.reelData.some(({ speed }) => speed > 0);
   }
 
   render() {
     const { reelData } = this.state;
 
     return (
-      <Container>
-        {reelData.map(({ position, slots }, i) => (
-          <Reel key={i} position={position} slots={slots} />
-        ))}
-      </Container>
+      <>
+        <ReelArea>
+          {reelData.map(({ position, slots }, i) => (
+            <Reel key={i} position={position} slots={slots} />
+          ))}
+        </ReelArea>
+        <Handle onStart={this.startSlotMachine} disabled={this.isRunning} />
+      </>
     );
   }
 }
