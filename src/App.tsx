fc7e5f49a8c3bc9f71ee2f1c7b2052ead4b3bfbd
Ville Vainio <ville.vainio@smartly.io>
2021-05-20 10:24:43 +0300

    remove placeholder code and add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Container = styled.div`
+  background-color: red;
+  width: 200px;
+  height: 200px;
+`;
+
+const App = () => {
+  return <Container>test</Container>;
+};
 
 export default App;
